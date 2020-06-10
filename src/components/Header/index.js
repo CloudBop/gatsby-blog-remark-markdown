dc9f92bddb54a03434962092e738e51ae9a102eb
Cloudbop <boppincolin@gmail.com>
2020-06-10 16:58:00 +0100

    added mobile menu and styled

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import Menu from 'components/Menu';
 import Hamburger from 'components/Hamburger';
+import MobileMenu from 'components/MobileMenu';
 
 // Hooks
 import { useSiteConfigQuery } from 'hooks/useSiteConfigQuery';
@@ -15,6 +16,7 @@ const Header = ({ siteTitle = `` }) => {
   return (
     <Wrapper>
       <Hamburger menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} items={siteConfig.menu} />
       <Menu items={siteConfig.menu} />
       <Link to="/">
         <Logo src={siteConfig.logo.publicURL} alt={siteTitle} />
