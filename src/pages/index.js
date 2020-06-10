1d395bdc36bf677a9a4e5c3206a0bee4c25efae0
Cloudbop <boppincolin@gmail.com>
2020-06-10 17:41:54 +0100

    added pageQuery for posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,50 @@ import React from 'react';
 
 import Layout from 'components/Layout';
 // import Image from 'components/image';
-// import SEO from 'components/seo';
+import SEO from 'components/SEO';
+import { graphql } from 'gatsby';
 
-const IndexPage = () => <Layout>Start Page</Layout>;
+const IndexPage = ({ data }) => (
+  <Layout>
+    {console.log('data', data)}
+    <SEO title="home" />
+    <div className="hero">hero goes here</div>
+    {/** wrap all posts here */}
+    <main>
+      <div className="blogpostcard">blog post paginate goes here</div>
+    </main>
+  </Layout>
+);
 
 export default IndexPage;
+
+export const indexQuery = graphql`
+  query GetPosts {
+    allMarkdownRemark(
+      filter: { frontmatter: { type: { eq: "post" } } }
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            readingTime {
+              text
+            }
+          }
+          frontmatter {
+            date
+            title
+            image {
+              childImageSharp {
+                fluid(maxWidth: 200, maxHeight: 200) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
