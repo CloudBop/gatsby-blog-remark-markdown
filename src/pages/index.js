496b753de4fbb9e94f1e9b1392317c935073a207
Cloudbop <boppincolin@gmail.com>
2020-06-10 18:20:28 +0100

    Setup Hero Background

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,14 @@ import React from 'react';
 import Layout from 'components/Layout';
 // import Image from 'components/image';
 import SEO from 'components/SEO';
+import Hero from 'components/Hero';
 import { graphql } from 'gatsby';
 
 const IndexPage = ({ data }) => (
   <Layout>
     {console.log('data', data)}
     <SEO title="home" />
-    <div className="hero">hero goes here</div>
+    <Hero />
     {/** wrap all posts here */}
     <main>
       <div className="blogpostcard">blog post paginate goes here</div>
